8c2e84f2db9d87fff2d2de7824f805e8683d87aa
kebaler <mjkeba@gmail.com>
2015-02-11 17:39:45 -0800

    adding twitter follower ids to csv script

---
+++ twitter_follower_ids_to_csv.py
@@ -0,0 +1,29 @@
+import yaml
+import tweepy
+import time
+import csv
+
+credentials = yaml.load(open('keeb_credentials.yaml'))
+auth = tweepy.OAuthHandler(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
+auth.set_access_token(credentials['OAUTH_TOKEN'], credentials['OAUTH_TOKEN_SECRET'])
+
+api = tweepy.API(auth)
+
+# variable for target screen name to get follower id list from 
+target_sn='DLSoftwareDeal'
+
+ids = []
+for page in tweepy.Cursor(api.followers_ids, screen_name=target_sn).pages():
+    ids.extend(page)
+    time.sleep(60)
+
+print len(ids)
+
+# creates csv named to target screen name 
+csvfile = target_sn + "_follower_ids.csv"
+
+# Write csv from ID list
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in ids:
+        writer.writerow([val])
